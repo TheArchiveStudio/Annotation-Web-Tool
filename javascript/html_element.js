404550e3381ba66ff66d6cb0cb4d2e0842b39f32
TheArchiveStudio <50602224+TheArchiveStudio@users.noreply.github.com>
2021-03-08 18:18:00 +0900

    Create html_element.js

---
+++ javascript/html_element.js
@@ -0,0 +1,36 @@
+class HtmlElement {
+	
+	// Create Element
+	// document.createElement("");
+	
+	// Insert Element
+	insertElement () {
+	}
+	
+	// Display Element
+	displayElement () {
+	}
+	
+	property () {
+	}
+	
+	hiddenElement () {
+	}
+	
+	deleteElement () {
+	}
+}
+
+class HtmlElementNode {
+	
+	// 부모
+	// 형제
+	// 이전 형제
+	// 다음 형제
+	// 첫번재 형제
+	// 마지막 형제
+}
+
+class HtmlElementProperty {
+
+}
