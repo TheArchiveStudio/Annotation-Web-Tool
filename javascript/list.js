11897d6036fdf0e175b78ff16f3b0ef7c1c4538d
TheArchiveStudio <50602224+TheArchiveStudio@users.noreply.github.com>
2021-03-08 10:57:01 +0900

    Update list.js

--- javascript/list.js
+++ javascript/list.js
@@ -2,17 +2,30 @@
 
 // LIST
 // Comment : 리스트 클래스.
-// Version : 1.0
-// Last Update Date : 2021-01-28
+// Version : 1.1
+// Last Update Date : 2021-03-06-03-47
 
-var List = function () {
+class List {
 	
-	this.list = [];
-	
-	// 리스트의 길이를 저장하는 변수
-	this.listSize = 0;
-	// 리스트 탐색 관련 기능을 위해 위치를 나타내는 변수
-	this.position = 0;
+	constructor (list) {
+		
+		if (list) {
+			
+				this.list = list;
+			
+				// 리스트의 길이를 저장하는 변수
+				this.listSize = list.length;
+			} else {
+			
+				this.list = [];
+			
+				// 리스트의 길이를 저장하는 변수
+				this.listSize = 0;
+			}
+		
+		// 리스트 탐색 관련 기능을 위해 위치를 나타내는 변수
+		this.position = 0;
+	}
 	
 	/**
 	 * 추가
@@ -21,18 +34,21 @@ var List = function () {
 	 */
 	
 	// 배열 뒤에 요소 추가하는 함수
-	this.append = function (element) {
+	append (element) {
 		this.list[this.listSize++] = element;
 	}
 	
 	// 요소를 원하는 위치에 추가하고 그 결과를 boolean 으로 반환하는 함수
-	this.insert = function (element, after){
+	insert (element, after) {
+		
 		var insertPosition = this.find(after);
+		
 		if(insertPosition > -1) {
 			this.list.splice(insertPosition+1, 0, element);
 			++this.listSize;
 			return true;
 		}
+		
 		return false;
 	}
 	
@@ -42,12 +58,12 @@ var List = function () {
 	 */
 	
 	// 리스트의 길이를 반환하는 함수
-	this.length = function (){
+	length (){
 		return this.listSize;
 	}
 	
 	// 리스트 요소를 확인하는 함수
-	this.toString = function () {
+	toString () {
 		return "["+this.list+"]";
 	}
 	
@@ -58,7 +74,7 @@ var List = function () {
 	 */
 	
 	// 리스트에 특정 요소가 있는지 그 결과를 boolean 으로 반환하는 함수
-	this.contains = function (element) {
+	contains (element) {
 		for(var i=0;i<this.list.length;i++) {
 			if(this.list[i] == element) {
 				return true;
@@ -68,7 +84,7 @@ var List = function () {
 	}
 	
 	// 요소의 index를 구하는 함수 (존재하지 않는 경우 -1 반환)
-	this.find = function (element) {
+	find (element) {
 		for(var i=0; i<this.list.length; i++) {
 			if(this.list[i] == element) {
 				return i;
@@ -90,43 +106,43 @@ var List = function () {
 	 */
 	
 	// index 로 요소를 반환하는 함수
-	this.get = function (position){
+	get (position){
 		return this.list[position];
 	}
 	
 	// position 이 보고 있는 현재 위치의 값을 반환 한다.
-	this.getElement = function (){
+	getElement (){
 		return this.list[this.position];
 	}
 	
 	// position 이 보고 있는 현재 위치를 반환 한다.
-	this.currPosition = function () {
+	currPosition () {
 		return this.position;
 	}
 	
 	// position 이 특정 위치를 보게 한다.
-	this.moveTo = function (position) {
+	moveTo (position) {
 		this.position = position;
 	} 
 	
 	// position 이 맨 앞을 보게 한다.
-	this.front = function () {
+	front () {
 		this.position = 0;
 	}
 	
 	// position 이 맨 뒤를 보게 한다.
-	this.end = function () {
+	end () {
 		this.position = this.listSize-1;
 	}
 	
 	// position 이 이전 위치를 보게 한다.
-	this.prev = function () {
+	prev () {
 		if(this.position > 0)
 			--this.position;
 	}
 	
 	// position 이 다음 위치를 보게 한다.
-	this.next = function () {
+	next () {
 		if(this.position < this.listSize-1) {
 			++this.position;
 		}
@@ -139,7 +155,7 @@ var List = function () {
 	 */	
 	
 	// 요소를 삭제하고 그 결과를 boolean 으로 반환하는 함수
-	this.remove = function (element){
+	remove (element){
 		var foundAt = this.find(element);
 
 		if(foundAt > -1) {
@@ -151,7 +167,7 @@ var List = function () {
 	}
 	
 	// 리스트의 모든 요소를 삭제하는 함수
-	this.clear = function () {
+	clear () {
 		delete this.list;
 		this.list.length = 0;
 		this.listSize = 0;
