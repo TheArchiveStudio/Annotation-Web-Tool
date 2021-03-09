b6900d6a46b62039e3d929b80c9d094193bbdf21
TheArchiveStudio <50602224+TheArchiveStudio@users.noreply.github.com>
2021-03-09 15:40:08 +0900

    Update html_element.js

--- javascript/html_element.js
+++ javascript/html_element.js
@@ -22,13 +22,19 @@ class HtmlElement {
 }
 
 class HtmlElementNode {
+
+	this.degree = 0; // 자식의 수
+
 	
-	// 부모
-	// 형제
+	// Parent Node 부모 
+	// Sibling Node 형제
 	// 이전 형제
 	// 다음 형제
 	// 첫번재 형제
 	// 마지막 형제
+
+	// external | leaf node 자식이 없는 노드.
+	// internal | branch node 차수가 0이 아닌 노드. 즉, 자식이 있는 노드.
 }
 
 class HtmlElementAttribute {
