a5bc7e22775cdd420d0147ecc308f5b3f5c83ece
TheArchiveStudio <50602224+TheArchiveStudio@users.noreply.github.com>
2021-03-10 11:41:56 +0900

    Update image-annotator.js

--- javascript/annotator/image-annotator.js
+++ javascript/annotator/image-annotator.js
@@ -7,24 +7,6 @@
 
 var coordinatePlane = new CoordinatePlane();
 
-window.onload = function() {
-	var htmlElementimageAannotatorWorkspaceSvg = document.getElementById("js-image-annotator__workspace__svg");
-	var imageAannotatorWorkspaceTargetImage = document.getElementById("image-annotator__workspace__target-image");
-	
-	htmlElementimageAannotatorWorkspaceSvg.onclick = function(e){
-		testOffsetXY();
-	}
-	
-	// Image Annotation Item List
-	var imageAnnotationItemList = new imageAnnotationItemList();
-	// + if Image Annotation Item List is Not Empty
-}
-
-funcion testOffsetXY (e) {
-	
-	alert("x : " + e.offsetX + " y : " + e.offsetY);
-}
-
 /* POINT
  * 1. Image Annotation Item Shape Point List : 한 아이템의 포인트 목록
  */
@@ -37,41 +19,40 @@ class imageAnnotationItemShapePointList {
 		this.item_id = item_id;
 		this.PointList = new List();
 	}
+	
+	// Set-Controll-Point__Xy
+
+	setDocumentElementControllPointXy (element, x, y) {
+		element.setAttribute("x", );
+		element.setAttribute("y", );
+	}
+
+	// Create-Controll-Point
+	createDocumentElementControllPoint () {
+		var element = document.createElement("use");
+		element.setAttribute("id", );
+		element.setAttribute("class", );
+	}
+
+	// Append-Controll-Point
+	appendDocumentElementControllPoint (id) {
+		id.appendChild();
+	}
+
+	// Dispaly-Controll-Point
+	dispalyDocumentElementControllPoint () {
+
+	}
+
+	hideDocumentElementControllPoint () {
+
+	}
+
+	// Delete-Document-Element__Controll-Point
+	deleteDocumentElementControllPoint () {
+
+	}
 }
-
-// Set-Controll-Point__Xy
-
-setDocumentElementControllPointXy (element, x, y) {
-	element.setAttribute("x", );
-	element.setAttribute("y", );
-}
-
-// Create-Controll-Point
-createDocumentElementControllPoint () {
-	var element = document.createElement("use");
-	element.setAttribute("id", );
-	element.setAttribute("class", );1
-}
-
-// Append-Controll-Point
-appendDocumentElementControllPoint (id) {
-	id.appendChild();
-}
-
-// Dispaly-Controll-Point
-dispalyDocumentElementControllPoint () {
-
-}
-
-hideDocumentElementControllPoint () {
-
-}
-
-// Delete-Document-Element__Controll-Point
-deleteDocumentElementControllPoint () {
-
-}
-
 
 function modifyItem () { }
 
